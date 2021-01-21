8f7a228f8e956a163abb488217155db6159f3b27
cjaurechi <cjaurechi@asapp.com>
2021-01-21 11:46:34 -0300

    Creación de componentes

--- src/components/ContentCardBig.js
+++ src/components/ContentCardBig.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import avatar from '../assets/images/dummy-avatar.jpg'
 
 function ContentCardBig () {
     return (
@@ -9,7 +10,7 @@ function ContentCardBig () {
                 </div>
                 <div class="card-body">
                     <div class="text-center">
-                        <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" src="assets/images/dummy-avatar.jpg" alt="dummy"/>
+                        <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" src={avatar} alt="dummy"/>
                     </div>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
                     <a target="_blank" rel="nofollow" href="/">View product detail</a>
