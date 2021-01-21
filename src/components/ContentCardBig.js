f44d2556d715dc58ca20f67ee8bce82a9101445b
cjaurechi <cjaurechi@asapp.com>
2021-01-21 16:19:16 -0300

    Práctica Props

--- src/components/ContentCardBig.js
+++ src/components/ContentCardBig.js
@@ -1,23 +1,48 @@
 import React from 'react'
 import avatar from '../assets/images/dummy-avatar.jpg'
+import PropTypes from 'prop-types'
 
-function ContentCardBig () {
+function ContentCardBig (props) {
+    let listadoEnlaces;
+    if(props.enlaces != null) {
+        listadoEnlaces = 
+        <ul>
+            {props.enlaces.map ( (enlace, i) => <li key={enlace + i} > {enlace} </li>)}
+        </ul>
+    } else {
+        listadoEnlaces = "Acá no hay nada";
+    }
+
     return (
         <div class="col-lg-6 mb-4">
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Last product in Data Dase</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">{props.titulo}</h6>
                 </div>
                 <div class="card-body">
                     <div class="text-center">
                         <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" src={avatar} alt="dummy"/>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
-                    <a target="_blank" rel="nofollow" href="/">View product detail</a>
+                    <p>{props.descripcion}</p>
+                    <a target="_blank" rel="nofollow" href="/">{props.link}</a>
+                    { listadoEnlaces }
                 </div>
             </div>
         </div>
     )
+}
+
+
+ContentCardBig.defaultProps = {
+    titulo: 'Hola',
+    enlaces: null
+}
+
+ContentCardBig.propTypes = {
+    titulo: PropTypes.string,
+    descripcion: PropTypes.string,
+    link: PropTypes.string,
+    enlaces: PropTypes.array
 }
 
 export default ContentCardBig;
